9c961540333fa7149a70aa67784b23c143277b5d
SkyzohKey <skyzohkey@protonmail.com>
2018-02-20 10:39:10 +0100

    Storybook actually now works & deploy via Travis once success.

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,23 +3,44 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
-import { Logo } from "../components/Logo";
-import { Button } from "../components/Button";
-import { Input } from "../components/Input";
+import Logo from "../components/Logo";
+import Button from "../components/Button";
+import Input from "../components/Input";
 
 storiesOf("Logo", module)
-  .add("with original size", () => <Logo size={"original"} />)
-  .add("with medium size", () => <Logo size={"medium"} />)
-  .add("with small size", () => <Logo size={"small"} />)
-  .add("with big size", () => <Logo size={"big"} />)
-  .add("with bigger size", () => <Logo size={"bigger"} />);
+  .add("with original size", () => (
+    <Logo
+      style={{ backgroundColor: "#414141", padding: 16 }}
+      size={"original"}
+    />
+  ))
+  .add("with medium size", () => (
+    <Logo style={{ backgroundColor: "#414141", padding: 16 }} size={"medium"} />
+  ))
+  .add("with small size", () => (
+    <Logo style={{ backgroundColor: "#414141", padding: 16 }} size={"small"} />
+  ))
+  .add("with big size", () => (
+    <Logo style={{ backgroundColor: "#414141", padding: 16 }} size={"big"} />
+  ))
+  .add("with bigger size", () => (
+    <Logo style={{ backgroundColor: "#414141", padding: 16 }} size={"bigger"} />
+  ));
 
 storiesOf("Button", module)
   .add("with text", () => (
-    <Button onClick={action("clicked")}>Hello Button</Button>
+    <Button
+      style={{ width: 100 }}
+      onPress={action("clicked")}
+      text="Hello Button"
+    />
   ))
   .add("with some emoji", () => (
-    <Button onClick={action("clicked")}>😀 😎 👍 💯</Button>
+    <Button
+      style={{ width: 95 }}
+      onPress={action("clicked")}
+      text="😀 😎 👍 💯"
+    />
   ));
 
 storiesOf("Input", module)
