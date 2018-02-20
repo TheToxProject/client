ac383b64704946aa5f92835c21c7235b82c69cdf
SkyzohKey <skyzohkey@protonmail.com>
2018-02-20 11:14:20 +0100

    Moooooar stories! :D

--- src/stories/index.js
+++ src/stories/index.js
@@ -6,6 +6,9 @@ import { linkTo } from "@storybook/addon-links";
 import Logo from "../components/Logo";
 import Button from "../components/Button";
 import Input from "../components/Input";
+import FormHeader from "../components/FormHeader";
+import LoginForm from "../components/LoginForm";
+import RegisterForm from "../components/RegisterForm";
 
 storiesOf("Logo", module)
   .add("with original size", () => (
@@ -60,4 +63,26 @@ storiesOf("Input", module)
       secureTextEntry={true}
       onChangeText={action("changed")}
     />
+  ));
+
+storiesOf("FormHeader", module)
+  .add("with base style", () => (
+    <FormHeader
+      style={{ backgroundColor: "#414141", padding: 16 }}
+      text={"My cool form header!"}
+    />
+  ))
+  .add("with some emoji", () => (
+    <FormHeader
+      style={{ backgroundColor: "#414141", padding: 16 }}
+      text={"😀 😎 👍 💯"}
+    />
+  ));
+
+storiesOf("Forms", module)
+  .add("Login form", () => (
+    <LoginForm style={{ backgroundColor: "#414141", padding: 16 }} />
+  ))
+  .add("Register form", () => (
+    <RegisterForm style={{ backgroundColor: "#414141", padding: 16 }} />
   ));
