d7c7d673e933e64ae478c8bf0ef796ffa8750598
SkyzohKey <skyzohkey@protonmail.com>
2018-02-20 11:27:25 +0100

    Update storybook webpack config to correctly load react-router-*

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -8,6 +8,23 @@ module.exports = (storybookBaseConfig, configType) => {
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loader: "file-loader"
+      },
+      {
+        test: /\.(js|jsx|mjs)$/,
+        include: [
+          /src\/*/,
+          /node_modules\/react-native-/,
+          /node_modules\/react-router*/
+        ],
+        loader: require.resolve("babel-loader"),
+        options: {
+          babelrc: false,
+          presets: [require.resolve("babel-preset-react-native")],
+          // ,'babel-preset-stage-0' 'babel-preset-es2015' @remove-on-eject-end This is a
+          // feature of `babel-loader` for webpack (not Babel itself). It enables caching
+          // results in ./node_modules/.cache/babel-loader/ directory for faster rebuilds.
+          cacheDirectory: false
+        }
       }
     ]
   };
